1edfbbff7367ff30efb30d58699c1ac0d8813133
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-08-03 19:51:56 +0200

    add modern normalize and prop-types

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,31 +1,39 @@
-import styles from './Profile.module.css'
+import styles from './Profile.module.css';
+import PropTypes from 'prop-types';
 
-export const Profile = ({username, tag, location, avatar, stats}) => {
+export const Profile = ({ username, tag, location, avatar, stats }) => {
   const profileClasses = [styles.profile];
 
-return(
-  
-  <div className={profileClasses}>
-    <div>
-      <img src={avatar} alt={username} />
-      <p> {username} </p>
-      <p> @{tag} </p>
-      <p> {location} </p>
+  return (
+    <div className={profileClasses}>
+      <div>
+        <img src={avatar} alt={username} />
+        <p> {username} </p>
+        <p> @{tag} </p>
+        <p> {location} </p>
+      </div>
+      <ul>
+        <li>
+          <span> Followers </span>
+          <span> {stats.followers} </span>
+        </li>
+        <li>
+          <span> Views </span>
+          <span> {stats.views} </span>
+        </li>
+        <li>
+          <span> Likes </span>
+          <span> {stats.likes} </span>
+        </li>
+      </ul>
     </div>
-    <ul>
-      <li>
-        <span> Followers </span>
-        <span> {stats.followers} </span>
-      </li>
-      <li>
-        <span> Views </span>
-        <span> {stats.views} </span>
-      </li>
-      <li>
-        <span> Likes </span>
-        <span> {stats.likes} </span>
-      </li>
-    </ul>
-  </div>
-);
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.object.isRequired,
 };
