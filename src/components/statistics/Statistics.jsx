f0e9daebfe1738b39340a4fdde3f7648910c8d03
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-08-04 22:11:45 +0200

    add Statistics.module.css

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,19 +1,29 @@
-// import styles from './Statistics.module.css';
+import styles from './Statistics.module.css';
 import PropTypes from 'prop-types';
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 export const Statistics = ({ titleStats, uploadStats }) => {
+  const { statistics, title, statList, item, label, percentage } = styles;
   return (
-    <div>
-      {titleStats && (<h2>titleStats</h2>)}
-      <ul>
+    <section className={statistics}>
+      {titleStats && <h2 className={title}>{titleStats}</h2>}
+      <ul className={statList}>
         {uploadStats.map(stats => (
-          <li key={stats.id}>
-            {stats.label}
-            {stats.percentage}
+          <li
+            className={item}
+            style={{ backgroundColor: `${getRandomHexColor()}`
+          }}
+            key={stats.id}
+          >
+            <span className={label}>{stats.label}</span>
+            <span className={percentage}>{stats.percentage}%</span>
           </li>
         ))}
       </ul>
-    </div>
+    </section>
   );
 };
 
